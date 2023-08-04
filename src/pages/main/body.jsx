7eec09ce4c43fa658ed29c1a0a71c030ac006aa1
양민석 <kurooru98@gmail.com>
2023-08-04 16:01:03 +0900

    가족 생성 페이지 framing (사진 업로드 시, 색 선택 시 기능 구현 미완)

--- src/pages/main/body.jsx
+++ src/pages/main/body.jsx
@@ -1,8 +1,57 @@
-import { TestText } from "./style";
-
+import { MainBody } from "./style";
 function Body() {
+
+    // 만들기 버튼 누를 시
+    const handleOnSubmit = (e) => {
+        e.preventDefault();
+        console.log(e);
+    }
+
+    // 컬러 선택 시
+    const handleOnClickColor = (e) => {
+        console.log(e);
+    }
     return (
-        <TestText>main page</TestText>
+        <MainBody>
+            <form onSubmit={handleOnSubmit}>
+                <p>가족 이름 : <input type="text" name="" id="" /></p>
+                <p>입장 번호 : <input type="password" name="" id="" /></p>
+                <p>입장 번호 확인 : <input type="password" name="" id="" /></p>
+                <p>내 이름 :  <input type="text" name="" id="" /></p>
+                <div>
+                    <p>내 사진 <span>(필수는 아니에요)</span></p> 
+                    <input 
+                    id="uploadFile" 
+                    type="file" 
+                    src="" 
+                    alt="" />
+                    <label htmlFor="uploadFile">사진올리기 <span>(클릭)</span></label>
+                    <div></div>
+                </div>
+                <div>
+                   <p>컬러 <span>(가족을 대표하는 색이 될거에요)</span></p>
+                   <div>
+                        <div>
+                            <input type="radio" name="color" id="#3CAEFF" />
+                            <label htmlFor="#3CAEFF" onClick={handleOnClickColor}></label>
+                            <input type="radio" name="color" id="#6DD66D" />
+                            <label htmlFor="#6DD66D" onClick={handleOnClickColor}></label>
+                            <input type="radio" name="color" id="#FFBCB9" />
+                            <label htmlFor="#FFBCB9" onClick={handleOnClickColor}></label>
+                        </div>
+                        <div>
+                            <input type="radio" name="color" id="#FFD732" />
+                            <label htmlFor="#FFD732" onClick={handleOnClickColor}></label>
+                            <input type="radio" name="color" id="#D873F1" />
+                            <label htmlFor="#D873F1" onClick={handleOnClickColor}></label>
+                            <input type="radio" name="color" id="#aaaaaa" />
+                            <label htmlFor="#aaaaaa" onClick={handleOnClickColor}></label>
+                        </div>
+                   </div>
+                </div>
+                <button type="submit">가족 만들기</button>
+            </form>
+        </MainBody>
     )
 }
 
