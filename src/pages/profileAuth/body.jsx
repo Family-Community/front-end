5ab7460e084e0432b18a883c36cce6468bf5bd00
양민석 <kurooru98@gmail.com>
2023-08-07 15:22:46 +0900

    profileAuth implement (framing)

--- src/pages/profileAuth/body.jsx
+++ src/pages/profileAuth/body.jsx
@@ -1,6 +1,18 @@
+import { ProfileAuthBody } from "./style";
 function Body() {
+    const handleOnSubmit = (e) => {
+        e.preventDefault();
+        console.log(e);
+    }
     return (
-        <p>profile Auth page</p>
+        <ProfileAuthBody>
+            <form onSubmit={handleOnSubmit}>
+                <p>호동이네 안방</p>
+                <p>입장번호를 입력해주세요</p>
+                <p><input type="password" name="" id="" placeholder="예) 1234"/></p>
+                <button type="submit">입장하기</button>
+            </form>
+        </ProfileAuthBody>
     )
 }
 
---
+++ src/pages/profileAuth/style.jsx
@@ -0,0 +1,44 @@
+import styled from "styled-components";
+
+export const ProfileAuthBody = styled.div`
+    
+    margin: 100px 0px;
+    
+    form{
+        p{
+            margin: 0 auto;
+            text-align: center;
+            font-size: 130%;
+            display: block;
+            width: 80%;
+            line-height: 2;
+        }
+        p:nth-child(1){
+            
+        }
+        p:nth-child(2){
+
+        }
+        p:nth-child(3){
+            margin-top: 10px;
+            input{
+                width: 60%;
+                text-align: center;
+                line-height: 2;
+                font-size: 110%;
+            }
+        }
+        button{
+            border: none;
+            color: #fff;
+            background-color: #aaaaaa;
+            display: block;
+            width: 30%;
+            line-height: 2.5;
+            border-radius: 10px;
+            font-size: 110%;
+            margin: 0 auto;
+            margin-top: 30px;
+        }
+    }
+`
