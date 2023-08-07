54b7c76142f413a253bfcfafa6adc7e600324aaf
양민석 <kurooru98@gmail.com>
2023-08-07 14:23:16 +0900

    add profileAuth

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Mypage from "./pages/mypage";
 import Profile from "./pages/profile";
 import ProfileAdd from "./pages/profileAdd";
 import ProfileEdit from "./pages/profileEdit";
+import ProfileAuth from "./pages/profileAuth";
 function App() {
   return (
     <Router>
@@ -15,6 +16,7 @@ function App() {
         <Route path={`/family_code/profile`} element={<Profile />}></Route>
         <Route path={`/profileAdd`} element={<ProfileAdd />}></Route>
         <Route path={`/profileEdit`} element={<ProfileEdit />}></Route>
+        <Route path={`/family_code/profileAuth`} element={<ProfileAuth />}></Route>
       </Routes>
     </Router>
   );
---
+++ src/pages/profileAuth/body.jsx
@@ -0,0 +1,7 @@
+function Body() {
+    return (
+        <p>profile Auth page</p>
+    )
+}
+
+export default Body;
