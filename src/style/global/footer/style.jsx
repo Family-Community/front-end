a1bb4335bc4974c4003ec0dbcac5806600e24e70
jimin1006 <129057172+jimin1006@users.noreply.github.com>
2023-08-07 10:35:30 +0900

    푸터수정

--- src/style/global/footer/style.jsx
+++ src/style/global/footer/style.jsx
@@ -4,9 +4,8 @@ export const FooterBox = styled.div`
     p{
         display: flex;
         justify-content: center;
-        margin-top: 13rem;
         margin-bottom: 0.5rem;
-        padding-top: 15rem;
+        padding-top: 1rem;
         font-size: 0.8rem;
     }
 `
