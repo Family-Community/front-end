1a7d13d9b175d3d75a4252f432225e3a761c8ded
jimin1006 <129057172+jimin1006@users.noreply.github.com>
2023-08-11 00:56:58 +0900

    api 연결 후 수정할 예정

---
+++ src/pages/mypage/body2.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+// import { Link } from 'react-router-dom';
+import { ImageContainer, MyPageHeaderBox, ButtonContainer } from "./style"
+import { MyPageBox, MainLink } from "./style";
+import profile from '../../assets/images/profile.svg';
+import photoex from '../../assets/images/photoex.png';
+import house from '../../assets/images/house-solid.svg';
+
+function Body() {
+
+    function MypageHeader() {
+        return (
+            <MyPageHeaderBox>
+                <MainLink to="../main">
+                    <img src={house}/>
+                </MainLink>
+                <h2>지민이네의 박지민</h2>
+            </MyPageHeaderBox>
+        )
+    }
+    return (
+        <div>
+            <MypageHeader />
+            <MyPageBox >
+                <img src={profile} />
+                <p>이름</p>
+                <p>삭제</p>
+                <p>|</p> 
+                <p>수정</p>
+                <img src={photoex} />
+                <p>제목</p>
+                <p>내용</p>
+                <p>😄</p>
+                <p>👍</p>
+                <p>😥</p>
+                <p>💗</p>
+                <p>😧</p>
+                <p>✔</p>
+            </MyPageBox>
+            <ButtonContainer>
+                <button type="submit">내 정보 수정</button>
+            </ButtonContainer>
+        </div>
+
+    )
+}
+
+export default Body;
