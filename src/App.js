9d4c74b9249853dbab4b2d64352b5548e847e234
양민석 <kurooru98@gmail.com>
2023-08-11 14:56:29 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,17 +6,19 @@ import Profile from "./pages/profile";
 import ProfileAdd from "./pages/profileAdd";
 import ProfileEdit from "./pages/profileEdit";
 import ProfileAuth from "./pages/profileAuth";
+import Post from "./pages/post";
 function App() {
   return (
     <Router>
       <Routes>
         <Route path={`/`} element={<Signup />}></Route>
-        <Route path={`/main`} element={<Main />}></Route>
+        <Route path={`/:familyCode`} element={<Main />}></Route>
         <Route path={`/mypage`} element={<Mypage />}></Route>
         <Route path={`/:family_code/profile`} element={<Profile />}></Route>
         <Route path={`/:family_code/profileAdd`} element={<ProfileAdd />}></Route>
         <Route path={`/profileEdit`} element={<ProfileEdit />}></Route>
         <Route path={`/:family_code/profileAuth`} element={<ProfileAuth />}></Route>
+        <Route path={`/:family_code/post`} element={<Post />}></Route>
       </Routes>
     </Router>
   );
