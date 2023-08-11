91a2f43f09d652f12980cfc829eb63c903a1ff04
양민석 <kurooru98@gmail.com>
2023-08-11 14:56:21 +0900

    create post page

---
+++ src/pages/post/body.jsx
@@ -0,0 +1,10 @@
+import { PostBody } from "./style";
+function Body() {
+    return (
+        <PostBody>
+            post Body
+        </PostBody>
+    )
+}
+
+export default Body;
---
+++ src/pages/post/index.jsx
@@ -0,0 +1,15 @@
+import Body from "./body";
+import Header from "../../style/global/header";
+import Footer from "../../style/global/footer";
+import Wrapper from "../../style/wrapper";
+function Post() {
+    return (
+        <Wrapper>
+            <Header></Header>
+            <Body></Body>
+            <Footer></Footer> 
+        </Wrapper>
+    )
+}
+
+export default Post;
