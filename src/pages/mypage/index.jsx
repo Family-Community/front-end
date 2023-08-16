e96030c9b38d2e507bd0f3ce81c6d5550d41490c
양민석 <kurooru98@gmail.com>
2023-08-16 16:22:03 +0900

    mypage framing

--- src/pages/mypage/index.jsx
+++ src/pages/mypage/index.jsx
@@ -5,7 +5,7 @@ import Wrapper from "../../style/wrapper";
 function mypage() {
     return (
         <Wrapper>
-            {/* <Header></Header> */}
+            <Header></Header>
             <Body></Body>
             <Footer></Footer>
         </Wrapper>
--- src/pages/mypage/style.jsx
+++ src/pages/mypage/style.jsx
@@ -1,198 +1,170 @@
-import { Link } from 'react-router-dom';
 import styled from "styled-components";
-import logo from "../../assets/images/logo.png";
-import profile from '../../assets/images/profile.svg';
 
-
-export const MyPageHeaderBox = styled.div`
-    width: 100%;
-    text-align: center;
-    margin: 0 ;
-    display: inline-block;
-    h2{
-        margin-bottom: 0;
-        margin-top: 0;
-        padding-right: 35px;
-
-    img {
-
-    }
-    }
-`
-
-export const MainLink = styled(Link)`
-    display: inline-block;
-    width: 30px;
-    height: 30px;
-    margin-left: 15px;
-    margin-top: 5px;
-    float: left;
-`;
-
-export const MyPageBox = styled.div`
-    border: 1px solid #ccc;
-    margin: 20px;
-    padding: 10px;
-    position: relative;
-
-    img:nth-child(1) {
-        width: 25px;
-        height: 25px;
-        margin: 0px;
-        text-align: left;
-        padding-top:3px;
-    }
-
-    p:nth-child(2){
-        margin: 0;
-        font-size: 100%;
-        line-height: 2.5;
-        display: inline-block;
-        width: 20%;
-        text-align: left;
-        padding-left: 5px;
-        padding-bottom: 5px;
-        height: 25px;
-    }
-
-    p:nth-child(3){
-        margin: 0;
-        font-size: 80%;
-        line-height: 2.5;
-        display: inline-block;
-        width: 8%;
-        text-align: center;
-        float: right;
-    }
-
-    p:nth-child(4){
-        margin: 0;
-        font-size: 80%;
-        line-height: 2.5;
-        display: inline-block;
-        width: 2%;
-        text-align: center;
-        float: right;
-    }
-
-    p:nth-child(5){
-        margin: 0;
-        font-size: 80%;
-        line-height: 2.5;
-        display: inline-block;
-        width: 8%;
-        text-align: center;
-        float: right;
-        padding-right: 0;
-    }
-
-    img:nth-child(6) {
-        width: 100%;
-        height: 100%;
-        object-fit: cover;
-        padding-top: 10px;
-    }
-
-    p:nth-child(7){
-        margin: 0;
-        font-size: 110%;
-        line-height: 2.5;
-        display: block;
-        width: 80%;
-        text-align: left;
-    }
-
-    p:nth-child(8){
-        margin: 0;
-        font-size: 90%;
-        line-height: 2.5;
-        display: block;
-        width: 80%;
-        text-align: left;
-    }
-
-    p:nth-child(9){
-        margin: 0;
-        font-size: 110%;
-        line-height: 2.5;
-        display: inline-block;
-        width: 14%;
-        text-align: center;
-        padding-left: 10px;
-    }
-
-    p:nth-child(10){
-        margin: 0;
-        font-size: 110%;
-        line-height: 2.5;
-        display: inline-block;
-        width: 13%;
-        text-align: center;
-        padding-left: 10px;
-    }
-
-    p:nth-child(11){
-        margin: 0;
-        font-size: 110%;
-        line-height: 2.5;
-        display: inline-block;
-        width: 13%;
-        text-align: center;
-        padding-left: 10px;
-    }
-
-    p:nth-child(12){
-        margin: 0;
-        font-size: 110%;
-        line-height: 2.5;
-        display: inline-block;
-        width: 13%;
-        text-align: center;
-        padding-left: 10px;
-    }
-
-    p:nth-child(13){
-        margin: 0;
-        font-size: 110%;
-        line-height: 2.5;
-        display: inline-block;
-        width: 13%;
-        text-align: center;
-        padding-left: 10px;
-    }
-
-    p:nth-child(14){
-        margin: 0;
-        font-size: 110%;
-        line-height: 2.5;
-        display: inline-block;
-        width: 13%;
-        text-align: center;
-        padding-left: 10px;
-    }
-`
-
-
-export const ButtonContainer = styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-
-    button {
-        appearance: none;
-        width: 30%;
-        line-height: 2;
-        font-size: 100%;
-        border: none;
-        background-color: #D5C2EE;
-        color: #8572EE;
-        opacity: 0.7;
-        cursor: pointer;
-        border-radius: 40px;
+export const MyPageBody = styled.div`
+    #userInfoBox{
+        display: flex;
+        width: 90%;
+        margin: 0 auto;
         margin-top: 20px;
-        transition: all 0.3s;
+        margin-bottom: 20px;
+        p{
+            height: 70px;
+            font-size: 120%;
+            font-weight: 700;
+        }
+        p:nth-child(1){
+            margin: 0;
+            width:calc(100% - 70px);
+            line-height: 70px;
+            text-align: center;
+            span{
+
+            }
+            span:nth-child(1){
+
+            }
+            span:nth-child(2){
+
+            }
+        }
+        p:nth-child(2){
+            width:70px;
+            margin: 0;
+            background-repeat: no-repeat;
+            background-position: center;
+            background-size: cover;
+            border-radius: 50%;
+            box-sizing: border-box;
+        }
+    }
+    #feedBox{
+        width: 80%;
+        margin: 0 auto;
+        height: 450px;
+        border: 2px solid red;
+        border-radius: 5px;
         box-sizing: border-box;
-        justify-items: center;
-        align-items: center;
+        overflow: scroll;
+        div{
+            margin: 0 auto;
+            border: 2px solid red;
+            box-sizing: border-box;
+            border-radius: 5px;
+            margin-top: 10px;
+            width: 90%;
+            div:nth-child(1){
+                margin: 0 auto;
+                height: 50px;
+                display: flex;
+                width: 90%;
+                justify-content: space-around;
+                border: none;
+                p{  
+                    margin: 0;
+                    line-height: 50px;
+                    height: 100%;
+                    text-align: center;
+                    font-size: 120%;
+                    font-weight: 700;
+                }
+                p:nth-child(1){
+                    display: block;
+                    width:50px;
+                }
+                p:nth-child(2){
+                    width: calc((100% - 50px) * 0.6);
+                    text-align: center;
+                }
+                p:nth-child(3){
+                    width: calc((100% - 50px) * 0.2);
+                    text-align: center;
+                }
+                p:nth-child(4){
+                    width: calc((100% - 50px) * 0.2);
+                    text-align: center;
+                }
+            }
+            div:nth-child(2){
+                width: 80%;
+                height: 200px;
+                border-radius: 5px;
+                box-sizing: border-box;
+                border: 2px solid red;
+                background-position: center;
+                background-repeat: no-repeat;
+                background-size: contain;
+                border: none;
+
+            }
+            div:nth-child(3){
+                width: 80%;
+                height: 150px;
+                margin-bottom: 10px;
+                border: none;
+            }
+            div:nth-child(4){
+                width: 100%;
+                height: 80px;
+                border: none;
+                div{
+                    display: flex;
+                    justify-content: space-around;
+                }
+                div:nth-child(1){
+                    width: 100%;
+                    height: 25px;
+                    p{
+                        width: 25px;
+                        height: 25px;
+                        margin: 0;
+                    }
+                    p:nth-child(1){
+                        
+                    }
+                    p:nth-child(2){
+
+                    }
+                    p:nth-child(3){
+
+                    }
+                    p:nth-child(4){
+
+                    }
+                    p:nth-child(5){
+
+                    }
+                    p:nth-child(6){
+
+                    }
+                }
+                div:nth-child(2){
+                    width: 100%;
+                    height: 25px;
+                    p{
+                        width: 25px;
+                        height: 25px;
+                        margin: 0;
+                    }
+                    p:nth-child(1){
+                        
+                    }
+                    p:nth-child(2){
+
+                    }
+                    p:nth-child(3){
+
+                    }
+                    p:nth-child(4){
+
+                    }
+                    p:nth-child(5){
+
+                    }
+                    p:nth-child(6){
+
+                    }
+                }
+            }
+        }
     }
 `
